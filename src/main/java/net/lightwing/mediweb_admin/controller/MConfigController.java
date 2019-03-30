60ece8832a3b58c7b150a9d97f45471166641683
tjtly <1058106949@qq.com>
2019-03-30 09:55:17 +0800

    站点管理

--- src/main/java/net/lightwing/mediweb_admin/controller/MConfigController.java
+++ src/main/java/net/lightwing/mediweb_admin/controller/MConfigController.java
@@ -13,46 +13,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class MConfigController
-{
+public class MConfigController {
 
     @Autowired
     private MConfigService service;
 
     @RequestMapping("config_manage.html")
-    public String ConfigManage(Model model, HttpSession session)
-    {
-        if(session.getAttribute("ADMIN")==null)
-        {
-            model.addAllAttributes(MessageBack.MSG(500,"请您重新登录。"));
+    public String ConfigManage(Model model, HttpSession session) {
+        if (session.getAttribute("ADMIN") == null) {
+            model.addAllAttributes(MessageBack.MSG(500, "请您重新登录。"));
             return "login.html";
-        }
-        else
-        {
-            try
-            {
-                MConfig data = service.getConfig(Constant.CONFIGID);
-                model.addAttribute("data",data);
-            }
-            catch (Exception e)
-            {
-                model.addAttribute("message",MessageBack.MSG(500,"系统基本信息获取失败"));
+        } else {
+            try {
+                if (service.count() != 0) {
+                    MConfig data = service.getConfig(Constant.CONFIGID);
+                    model.addAttribute("data", data);
+                } else {
+                    return "config_manage.html";
+                }
+
+            } catch (Exception e) {
+                model.addAttribute("message", MessageBack.MSG(500, "系统基本信息获取失败"));
             }
             return "config_manage.html";
         }
     }
 
     @RequestMapping("updateConfig")
-    public String updateConfig(@ModelAttribute MConfig config,Model model)
-    {
-        try
-        {
-            service.updateConfig(config);
-            model.addAttribute("message",MessageBack.MSG(200,"系统基本信息更新成功。"));
-        }
-        catch (Exception e)
-        {
-            model.addAttribute("message",MessageBack.MSG(500,"系统基本信息更新失败。"));
+    public String updateConfig(@ModelAttribute MConfig config, Model model) {
+        try {
+            if (config.getCid() == null) {
+                service.insertSelective(config);
+            } else {
+                service.updateConfig(config);
+            }
+
+            model.addAttribute("message", MessageBack.MSG(200, "系统基本信息更新成功。"));
+        } catch (Exception e) {
+            model.addAttribute("message", MessageBack.MSG(500, "系统基本信息更新失败。"));
         }
         return "config_manage.html";
     }
